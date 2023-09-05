532a823e6dfcdd49da0d58b6babb9b2582d0234d
ph-dev-labs <85322356+ph-dev-labs@users.noreply.github.com>
2023-09-05 19:10:49 +0100

    displayed cart items

--- src/components/cartCheckout/Checkout.jsx
+++ src/components/cartCheckout/Checkout.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 import "./Checkout.css";
-const Checkout = ({ modalRef, closeModal, cartItems, price }) => {
-  const cartItem = cartItems.map((item) => {
-    return (
-      <div className="cart-item">
-        <span>{item.item}</span>
-        <span>{item.price}</span>
+
+const Checkout = ({ modalRef, closeModal, cartItems }) => {
+  // Calculate the total price of all items in the cart
+  const total = cartItems.reduce((acc, item) => {
+    return acc + item.price * item.quantity;
+  }, 0);
+
+  // Render each item in the cart with quantity and subtotal
+  const cartItemsList = cartItems.map((item) => (
+    <div key={item.id} className="cart-item">
+      <div className="item-details">
+        <span className="item-name text">{item.item}</span>
+        <span className="item-price text">${item.price}</span>
       </div>
-    );
-  });
+      <div className="item-quantity">
+        <span className="text-1">Quantity: {item.quantity}</span>
+        <span className="text-1">Subtotal: ${item.price * item.quantity}</span>
+      </div>
+    </div>
+  ));
 
   return (
     <div className="cart-checkout-item" ref={modalRef}>
       {/* Display cart items, total, and checkout button */}
-      <div className="cart-items">{/* List of cart items */}
-         {cartItem}
-      </div>
+      <div className="cart-items text">{cartItemsList}</div>
       <div className="cart-total">
-        <span>Total: $00.00</span>
+        <span className="text">Total: ${ total.toFixed(2)}</span>
       </div>
       <button className="checkout-button" onClick={closeModal}>
-        close
+        Close
       </button>
     </div>
   );
